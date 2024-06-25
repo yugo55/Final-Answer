ea404b4493cf03c5d1f0565c2c301611ec642a34
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-06-25 18:33:49 +0900

    機能完成

---
+++ python/ex1_web-scraping/1-1.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+import csv
+import socket
+import ssl
+import urllib
+
+headers = {
+  "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+}
+
+top_url = "https://r.gnavi.co.jp/area/jp/rs/?date=20240624"
+res = requests.get(top_url, headers=headers)
+html = BeautifulSoup(res.content, "html.parser")
+
+# 1ページ目の店の店舗urlを取得
+restaurants_a = html.select(".style_restaurant__SeIVn a.style_titleLink__oiHVJ")
+restaurants_url_array = [a.get("href") for a in restaurants_a]
+
+# 2ページ目以降の店の店舗urlを取得
+i = 2
+while len(restaurants_url_array) < 50:
+  url = f"https://r.gnavi.co.jp/area/jp/rs/?date=20240624&p={i}"
+  time.sleep(3)
+  res = requests.get(url, headers=headers)
+  html = BeautifulSoup(res.content, "html.parser")
+  restaurants_a = html.select(".style_restaurant__SeIVn a.style_titleLink__oiHVJ")
+  for a in restaurants_a:
+    restaurants_url_array.append(a.get("href"))
+    if len(restaurants_url_array) == 50:
+      break
+  i += 1
+
+with open("1-1.csv", "a") as f:
+  writer = csv.writer(f)
+  writer.writerow(["店舗名","電話番号","メールアドレス","都道府県","市区町村","番地","建物名","URL","SSL"])
+  for url in restaurants_url_array:
+    print(url)
+    time.sleep(3)
+    res = requests.get(url, headers=headers)
+    html = BeautifulSoup(res.content, "html.parser")
+    name = html.select_one("p.fn").text
+    phone_num = html.select_one("span.number").text
+    all_table_a = html.select(".basic-table a")
+    email = ""
+    for a in all_table_a:
+      if "mailto" in a.get("href"):
+        email = a.get("href").replace("mailto:", "")
+    address = html.select_one("span.region").text
+    pref = re.match(r"^.+(都|道|府|県)", address).group()
+    city = re.search(r"(?<=都|道|府|県)\D+", address).group()
+    street_address = re.search(r"\d+(-|\d)+$", address).group() if re.search(r"\d+(-|\d)+", address) is not None else ""
+    building_ele = html.select_one("span.locality")
+    building = building_ele.text if building_ele else ""
+    restaurant_url = ""
+    parsed_url = urllib.parse.urlparse(url)
+    hostname = parsed_url.hostname
+    port = parsed_url.port if parsed_url.port else 443
+    try:
+      context = ssl.create_default_context()
+      with socket.create_connection((hostname, port)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+          cert = ssock.getpeercert()
+          if cert:
+            has_ssl = True
+    except ssl.SSLError:
+      has_ssl = False
+    except Exception as e:
+      print(f"Error: {e}")
+      has_ssl = False
+    writer.writerow([name, phone_num, email, pref, city, street_address, building, restaurant_url, has_ssl])
